d188a1d0949a816ed34eb6ad111f8850527a00cd
Luana Savian <luana.savian@zup.com.br>
2021-08-27 14:01:20 -0300

    Métricas prometheus

--- src/main/java/br/com/zup/luanasavian/proposta/config/SecurityConfig.java
+++ src/main/java/br/com/zup/luanasavian/proposta/config/SecurityConfig.java
@@ -6,12 +6,14 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private static final String PROPOSTAS = "/propostas/**";
     private static final String CARTOES = "/cartoes/**";
+    private static final String ACTUATOR= "/actuator/**";
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -19,7 +21,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.GET, PROPOSTAS).hasAuthority("SCOPE_escopo-proposta")
                 .antMatchers(HttpMethod.GET, CARTOES).hasAuthority("SCOPE_escopo-proposta")
                 .antMatchers(HttpMethod.POST, PROPOSTAS).hasAuthority("SCOPE_escopo-proposta")
+                .antMatchers(HttpMethod.GET, ACTUATOR).permitAll()
                 .anyRequest().authenticated()
         ).oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 }
